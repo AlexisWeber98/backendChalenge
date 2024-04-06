48ff6bd3d3ecda2fd36adcbc16f360777320b773
Ale David Weber <Alexis.Weber@mailfence.com>
2024-04-06 16:07:34 -0300

    add delete by id cntroller

--- src/controllers/delete.controller.ts
+++ src/controllers/delete.controller.ts
@@ -1,15 +1,19 @@
 import type { Request, Response } from "express";
 import productModel from "../models/Product";
 
-const deleteProduct = async (req:Request, res:Response) => {
-try {
+export const deleteProduct = async (req: Request, res: Response) => {
+  try {
     const id = req.params.id;
-    // const deleteProduct = await productModel.delete({_id : id});
-    //  if(!deleteProduct){
-    //      return res.status(401).json('Error while deleting the product');
-    //  }else{
-        return res.status(200).json("Successfully deleted the product");
-} catch (error) {
-    
+    const deletedProduct = await productModel.findByIdAndDelete(id);
+    if (!deletedProduct) {
+      return res.status(404).json({ error: "Product not found" });
+    } else {
+      return res.status(200).json({ message: "Successfully deleted the product" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
 
-}}
+export default  deleteProduct;
+
