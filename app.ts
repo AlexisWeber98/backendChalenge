62b63f7edccbf6c4aeab4fe7359ff526a720c8a9
Ale David Weber <Alexis.Weber@mailfence.com>
2024-04-06 09:34:20 -0300

    add server app

---
+++ app.ts
@@ -0,0 +1,10 @@
+const express =require ("express");
+const morgan = require ("morgan");
+import router from "./routes/routes";
+
+const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use("/", router);
+
+export default app;
