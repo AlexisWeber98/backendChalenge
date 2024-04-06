9695f0cfee726b06da0beae3275446615aae09e1
Ale David Weber <Alexis.Weber@mailfence.com>
2024-04-06 16:07:55 -0300

    route delete controller

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
-
 import { postProduct } from "../controllers/post.controller";
 import { getAllProducts } from "../controllers/get.controllers";
+import deleteProduct from "../controllers/delete.controller";
 const router = Router();
 
 // -------------- post --------------- //
 router.post("/product", postProduct);
 // -------------- get ---------------- //
-router.get("/product", getAllProducts)
+router.get("/product", getAllProducts);
+// -------------- delete ------------- //
+router.delete("/product/:id", deleteProduct)
 
 export default router;
