d76785df8a259ae052752a6c54c0e6225921ee7e
Ale David Weber <Alexis.Weber@mailfence.com>
2024-04-06 16:47:23 -0300

    put route

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { postProduct } from "../controllers/post.controller";
 import { getAllProducts } from "../controllers/get.controllers";
+import putProduct from "../controllers/put.controller";
 import deleteProduct from "../controllers/delete.controller";
 const router = Router();
 
@@ -10,5 +11,6 @@ router.post("/product", postProduct);
 router.get("/product", getAllProducts);
 // -------------- delete ------------- //
 router.delete("/product/:id", deleteProduct)
-
+// -------------- put ---------------- //
+router.put("/product", putProduct)
 export default router;
