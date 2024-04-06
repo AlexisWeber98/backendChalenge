db486d080da32803f8a77e3e08217983224a81bd
Ale David Weber <Alexis.Weber@mailfence.com>
2024-04-06 14:58:44 -0300

    create functions order, pag, search by title, search by id

--- src/controllers/get.controllers.ts
+++ src/controllers/get.controllers.ts
@@ -3,9 +3,55 @@ import productModel from "../models/Product";
 
 const getAllProducts = async (req: Request, res: Response) => {
   try {
-    const getProduct = await productModel.find();
-    res.status(200).json(getProduct);
-  } catch (error) {}
+    const { id, title, price, page, order } = req.query;
+
+    // ---------------- search by id ---------------//
+
+    if (id) {
+      let result = await productModel.findById(String(id));
+      if (!result) return res.status(404).json({ message: "No Product Found" });
+      return res.status(200).json(result);
+    }
+    let query: any = {};
+
+    // -------------------- options --------------------- //
+
+
+    if(title){
+        let  regex : RegExp= new RegExp(String(title), 'i');  
+       query.title = regex ;  
+    }
+
+    if (price) query.price = { $lte: price };
+
+    let sort: any = {};
+    if (order) {
+      const sortOrder = order === "asc" ? 1 : -1;
+      sort.price = sortOrder;
+    }
+
+    const itemsPerPage = 8;
+    const currentPage = parseInt(page as string) || 1;
+    const offset = (currentPage - 1) * itemsPerPage;
+
+    const products = await productModel
+      .find(query)
+      .sort(sort)
+      .skip(offset)
+      .limit(itemsPerPage);
+
+    const totalProducts = await productModel.countDocuments(query);
+
+    const totalPages = Math.ceil(totalProducts / itemsPerPage);
+    const response = {
+      products,
+      totalPages,
+    };
+
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).send({ error });
+  }
 };
 
 export { getAllProducts };
