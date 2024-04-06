5052e81d8932906655c51a87510724adeab3960e
Ale David Weber <Alexis.Weber@mailfence.com>
2024-04-06 09:54:20 -0300

    delete database file and move code to index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 import app from "./app";
-require('/database.ts')
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb+srv://Ale:Todoslosfuegoselfuego@mongodatabase.3uheaaw.mongodb.net/', {
+}).then(() => console.log('Conexión exitosa a MongoDB'))
+  .catch(err => console.error('Error al conectar a MongoDB', err));
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error de conexión:'));
+db.once('open', () => {
+  console.log('Conexión exitosa a la base de datos');
+});
+
 
 app.listen(3001, () => {
+
 console.log("Serven listen on port 3001");
 
 })
