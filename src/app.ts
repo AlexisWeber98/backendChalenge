4575a364c415a91318081113e478ff687bf0dd80
Ale David Weber <Alexis.Weber@mailfence.com>
2024-04-07 22:30:30 -0300

    add cors policity

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,31 @@
-const express =require ("express");
-const morgan = require ("morgan");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 import router from "./routes/index.routes";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import type { Request, Response, NextFunction } from "express";
 
 const app = express();
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+// });
 app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(morgan("dev"));
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 app.use("/", router);
 
 export default app;
